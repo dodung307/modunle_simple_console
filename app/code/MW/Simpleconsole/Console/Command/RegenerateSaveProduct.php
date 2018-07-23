44406bd5d3ab79bb4999d6855d266a75fa0da92f
dodung307 <anhdungk7@gmail.com>
2018-07-23 18:03:19 +0700

    [simpleconsole]

--- app/code/MW/Simpleconsole/Console/Command/RegenerateSaveProduct.php
+++ app/code/MW/Simpleconsole/Console/Command/RegenerateSaveProduct.php
@@ -75,24 +75,26 @@ class RegenerateSaveProduct extends Command
             $numberProduct = $input->getOption(self::INPUT_NUMBER_PRODUCT);
         }
 
-        for($i = 1; $i <= $numberProduct; $i++ ){
-            try {
-                $product = $this->_objectManager->get('Magento\Catalog\Model\Product');
-                $product
-                    ->setName('Random name ' . time())
-                    ->setSku(time())
-                    ->setPrice(0)
-                    ->setStatus(2)// Disabled
-                    ->setWebsiteIds([1, 2])
-                    ->setVisibility(4)
-                    ->setAttributeSetId($product->getDefaultAttributeSetId())
-                    ->setTypeId('simple')
-                    ->save();
-            } catch (\Exception $e) {
-                // debugging
-                $output->writeln($e->getMessage());
-            }
-        }
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // instance of object manager
+        $product = $objectManager->create('\Magento\Catalog\Model\Product');
+        $product->setSku('my-sku'); // Set your sku here
+        $product->setName('Sample Simple Product'); // Name of Product
+        $product->setAttributeSetId(4); // Attribute set id
+        $product->setStatus(1); // Status on product enabled/ disabled 1/0
+        $product->setWeight(10); // weight of product
+        $product->setVisibility(4); // visibilty of product (catalog / search / catalog, search / Not visible individually)
+        $product->setTaxClassId(0); // Tax class id
+        $product->setTypeId('simple'); // type of product (simple/virtual/downloadable/configurable)
+        $product->setPrice(100); // price of product
+        $product->setStockData(
+            array(
+                'use_config_manage_stock' => 0,
+                'manage_stock' => 1,
+                'is_in_stock' => 1,
+                'qty' => 999999999
+            )
+        );
+        $product->save();
 
         $output->writeln('');
         $output->writeln('');
